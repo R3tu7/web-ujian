f08549d34156143733e7ac681d2871fb4991724e
R3tu7 <ratuaziz38@gmail.com>
2024-11-20 20:26:02 +0700

    first commit

---
+++ latihan.js
@@ -0,0 +1,9 @@
+var nilaiawal = 1;
+while( nilaiawal <= 5) {
+    console.log('angkot no.' + nilaiawal + 'lagi beroperasi');
+  nilaiawal ++;  
+}
+for( nilaiawal = 6; nilaiawal <=10; nilaiawal++ ){
+    console.log('angkot no.' + nilaiawal + 'tidak beroperasi')
+
+}
